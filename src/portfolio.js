360d89bfa7c0fe0ca5a1bdcfeed956655044f9a6
vinniedaboi <kidosol5224@gmail.com>
2023-05-10 12:57:36 +0800

    Changed blog section

--- src/portfolio.js
+++ src/portfolio.js
@@ -322,18 +322,17 @@ const achievementSection = {
 // Blogs Section
 
 const blogSection = {
-  title: "Blogs",
+  title: "Blogs and Researchs",
   subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
-  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+    "Some of my research papers and blogs",
+  displayMediumBlogs: "false", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://medium.com/@kidosol5224/making-your-first-cinematic-in-ue5-916241b77a87",
+      title: "Making your first UE5 cinematic",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+        "Make your first cinematic in UE5 with a detailed tutorial using blueprints"
     },
-
   ],
   display:true // Set false to hide this section, defaults to true
 };
